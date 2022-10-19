e0e676c77fad5155ea5983b1a178036e7dd8eaea
Vladyslav Drushliak <vladislav.drushliak@gmail.com>
2022-10-19 13:30:26 +0300

    Create and configure a project

---
+++ EventsLINQ/EventsLINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventsLINQ
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var task1 = new Task1();
+        }
+    }
+}
