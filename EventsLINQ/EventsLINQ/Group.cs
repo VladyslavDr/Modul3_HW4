450223a111dd58cd95c9e7fccb14c2bd5d788619
Vladyslav Drushliak <vladislav.drushliak@gmail.com>
2022-10-20 15:48:20 +0300

    Task2

---
+++ EventsLINQ/EventsLINQ/Group.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsLINQ
+{
+    public enum Group
+    {
+        Emergency,
+        Colleagues,
+        Family,
+        Friends
+    }
+}
--- EventsLINQ/EventsLINQ/Program.cs
+++ EventsLINQ/EventsLINQ/Program.cs
@@ -9,6 +9,10 @@ namespace EventsLINQ
             var task1 = new Task1();
 
             task1.Run(() => Console.WriteLine(task1.Res(2, 4)));
+
+            var task2 = new Task2();
+
+            task2.Run();
         }
     }
 }
